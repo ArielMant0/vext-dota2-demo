3e0f212a015a1cbdb24eec1708b1a469eab96fc7
Franziska Becker <franziska.becker@vis.uni-stuttgart.de>
2023-06-10 20:21:29 +0200

    added correct server base

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ import { fileURLToPath, URL } from 'node:url'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue({ 
+    vue({
       template: { transformAssetUrls }
     }),
     // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
@@ -35,7 +35,17 @@ export default defineConfig({
       '.vue',
     ],
   },
+  build: {
+    rollupOutputOptions: {
+      output: {
+        entryFileNames: "[name].js",
+        chunkFileNames: "[name].js",
+        assetFileNames: "[name].[extname]"
+      }
+    }
+  },
   server: {
     port: 3000,
+    base: "/vext-dota2-demo/",
   },
 })
